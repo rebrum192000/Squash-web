a9ab1417aa7b1158216b7d7fb0f782d972754aa4
Konstantin <rebrum19200@gmail.com>
2019-05-22 16:48:47 +0300

    Add files via upload

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,9 +4,16 @@ var mOpenBtn = document.querySelector('.first--btn');
 var mOpenBtn2 = document.querySelector('.play--btn');
 var mOpenBtn3 = document.querySelector('.sect10__book');
 var mCloseBtn = document.querySelector('.modal--close-btn');
+var youtube = document.querySelector('.youtube');
+var youtubeOpenBtn = document.querySelector('.first--watch-btn');
+var youtubeCloseBtn = document.querySelector('.youtube--close-btn');
 
 mOpenBtn.onclick = function() {
 	modal.setAttribute("style", "display:block");
+	modalShadow.setAttribute("style", "display:block");
+};
+youtubeOpenBtn.onclick = function() {
+	youtube.setAttribute("style", "display:block");
 	modalShadow.setAttribute("style", "display:block");
 };
 mOpenBtn2.onclick = function() {
@@ -21,7 +28,12 @@ mCloseBtn.onclick = function() {
 	modal.setAttribute("style", "display:none");
 	modalShadow.setAttribute("style", "display:none");
 };	
+youtubeCloseBtn.onclick = function() {
+	youtube.setAttribute("style", "display:none");
+	modalShadow.setAttribute("style", "display:none");
+};
 modalShadow.onclick = function() {
+	youtube.setAttribute("style", "display:none");
 	modal.setAttribute("style", "display:none");
 	modalShadow.setAttribute("style", "display:none");
 };	
