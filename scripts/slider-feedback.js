a716a8548e594e87ae3f763d678c4e24171c13f2
Konstantin <rebrum19200@gmail.com>
2019-05-23 13:24:20 +0300

    Add files via upload

--- scripts/slider-feedback.js
+++ scripts/slider-feedback.js
@@ -34,45 +34,46 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = "block";
 }
-}   else {
-    /* Индекс слайда по умолчанию */
-    /* Индекс слайда по умолчанию */
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-    /* Функция увеличивает индекс на 1, показывает следующй слайд*/
-    function plusSlide() {
-        showSlides(slideIndex += 1);
-    }
-
-    /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-    function minusSlide() {
-        showSlides(slideIndex -= 1);  
-    }
-
-    /* Устанавливает текущий слайд */
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    /* Основная функция слайдера */
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("feedback__item-pair");
-        var dots = document.getElementsByClassName("feedback-button");
-        if (n > slides.length) {
-          slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace("feedback-button-active", "");
-        }
-        slides[slideIndex - 1].style.display = "flex";
-        dots[slideIndex - 1].className += " feedback-button-active";
-    }
 }
+// }   else {
+//     /* Индекс слайда по умолчанию */
+//     /* Индекс слайда по умолчанию */
+//     var slideIndex = 1;
+//     showSlides(slideIndex);
+
+//     /* Функция увеличивает индекс на 1, показывает следующй слайд*/
+//     function plusSlide() {
+//         showSlides(slideIndex += 1);
+//     }
+
+//     /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+//     function minusSlide() {
+//         showSlides(slideIndex -= 1);  
+//     }
+
+//     /* Устанавливает текущий слайд */
+//     function currentSlide(n) {
+//         showSlides(slideIndex = n);
+//     }
+
+//     /* Основная функция слайдера */
+//     function showSlides(n) {
+//         var i;
+//         var slides = document.getElementsByClassName("feedback__item-pair");
+//         var dots = document.getElementsByClassName("feedback-button");
+//         if (n > slides.length) {
+//           slideIndex = 1
+//         }
+//         if (n < 1) {
+//             slideIndex = slides.length
+//         }
+//         for (i = 0; i < slides.length; i++) {
+//             slides[i].style.display = "none";
+//         }
+//         for (i = 0; i < dots.length; i++) {
+//             dots[i].className = dots[i].className.replace("feedback-button-active", "");
+//         }
+//         slides[slideIndex - 1].style.display = "flex";
+//         dots[slideIndex - 1].className += " feedback-button-active";
+//     }
+// }
